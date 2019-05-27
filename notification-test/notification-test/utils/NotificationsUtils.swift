62d377176ff8d2c672e78d708f543096890018a3
Frederico Braga <frederico.braga@x-team.com>
2019-05-28 06:03:29 +1000

    1st commit

---
+++ notification-test/notification-test/utils/NotificationsUtils.swift
@@ -0,0 +1,38 @@
+//
+//  NotificationsUtils.swift
+//  notification-test
+//
+//  Created by Fred on 28/05/19.
+//  Copyright © 2019 Fred. All rights reserved.
+//
+
+import Foundation
+import UserNotifications
+
+class NotificationsUtils{
+    
+    static func requestAuthorization(){
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+            if(granted){
+                print("granted")
+                print(granted)
+            }else if(error != nil){
+                print("error")
+                print(error?.localizedDescription as Any)
+            }
+        }
+    }
+    
+    static func setCategory(){
+        //https://developer.apple.com/library/archive/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/SupportingNotificationsinYourApp.html#//apple_ref/doc/uid/TP40008194-CH4-SW1
+        let generalCategory = UNNotificationCategory(identifier: "GENERAL",
+                                                     actions: [],
+                                                     intentIdentifiers: [],
+                                                     options: .customDismissAction)
+        
+        let center = UNUserNotificationCenter.current()
+        center.setNotificationCategories([generalCategory])
+    }
+}
+
---
+++ notification-test/notification-testTests/notification_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  notification_testTests.swift
+//  notification-testTests
+//
+//  Created by Fred on 28/05/19.
+//  Copyright © 2019 Fred. All rights reserved.
+//
+
+import XCTest
+@testable import notification_test
+
+class notification_testTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
